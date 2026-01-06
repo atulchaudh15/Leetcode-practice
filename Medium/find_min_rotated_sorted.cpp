dacc58afde89ecc66cc0e243775e1c00d6e58bc0
atulchaudh15 <atulchaudh9450@gmail.com>
2026-01-06 18:58:02 +0530

    Solved 5th ques of bs playlist

--- Medium/find_min_rotated_sorted.cpp
+++ Medium/find_min_rotated_sorted.cpp
@@ -1,4 +1,31 @@
 
+   //LEETCODE-> 151 min in sorted rotated array
 
+#include<bits/stdc++.h>
+using namespace std;
 
-//take it and eliminate the right half
+int findMin(vector<int>& nums) {
+        int mini=INT_MAX;
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            //check if left half is sorted
+            if(nums[low]<=nums[mid]){
+                //if the left half is sorted then low
+                //will be the smallest element
+                mini= min(mini,nums[low]);
+                //take it and eliminate the left half
+                low=mid+1;
+            }else{
+                //right half is sorted
+                //then mid will be the smallest element
+                mini= min(mini,nums[mid]);
+                //take it and eliminate the right half
+                high=mid-1;  
+            }
+        }
+        return mini;
+    }
+//TC->0(log n)
+//SC->0(1)
